c91ce907687c0c134d4ff3ee08e9b6bae82cd63b
VuNgN <vunguyen220901@gmail.com>
2023-05-10 09:29:00 +0700

    Refactor main screen: add bottom navigation bar.

--- app/src/main/java/com/vungn/mytlumdc/ui/home/MainFragment.kt
+++ app/src/main/java/com/vungn/mytlumdc/ui/home/MainFragment.kt
@@ -6,16 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.vungn.mytlumdc.R
 import com.vungn.mytlumdc.databinding.FragmentMainBinding
-import com.vungn.mytlumdc.ui.home.adapter.ViewpagerAdapter
-import com.vungn.mytlumdc.ui.home.views.FeatureFragment
-import com.vungn.mytlumdc.ui.home.views.HomeFragment
-import com.vungn.mytlumdc.ui.home.views.NotificationFragment
-import com.vungn.mytlumdc.ui.home.views.SettingFragment
 import com.vungn.mytlumdc.ui.login.LoginFragment
 import com.vungn.mytlumdc.ui.user.UserViewModel
 import com.vungn.mytlumdc.ui.user.impl.UserViewModelImpl
@@ -25,7 +21,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
-    private lateinit var viewpagerAdapter: ViewpagerAdapter
     private val userViewModel: UserViewModel by activityViewModels<UserViewModelImpl>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,55 +47,19 @@ class MainFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val homeFragment = HomeFragment()
-        val featureFragment = FeatureFragment()
-        val notificationFragment = NotificationFragment()
-        val settingFragment = SettingFragment()
-        val navController = findNavController()
+        val bottomNavBarFragment =
+            childFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+        val bottomNavBarController = bottomNavBarFragment.navController
+        binding.navigationBar.setupWithNavController(navController = bottomNavBarController)
+
+        val navHostController = findNavController()
         userViewModel.user.observe(viewLifecycleOwner) { user ->
             if (user != null) {
                 Toast.makeText(this.context, "Login success", Toast.LENGTH_SHORT).show()
             } else {
-                navController.navigate(R.id.loginFragment)
+                navHostController.navigate(R.id.loginFragment)
             }
         }
-        loadFragment(homeFragment)
-        viewpagerAdapter = ViewpagerAdapter(this)
-        binding.navigationBar.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.homeFragment -> {
-                    loadFragment(homeFragment)
-                    true
-                }
-
-                R.id.featureFragment -> {
-                    loadFragment(featureFragment)
-                    true
-                }
-
-                R.id.notificationFragment -> {
-                    loadFragment(notificationFragment)
-                    true
-                }
-
-                R.id.settingFragment -> {
-                    loadFragment(settingFragment)
-                    true
-                }
-
-                else -> {
-                    false
-                }
-            }
-        }
-    }
-
-    private fun loadFragment(fragment: Fragment) {
-        val transaction: FragmentTransaction =
-            requireActivity().supportFragmentManager.beginTransaction()
-        transaction.replace(binding.fragment.id, fragment)
-        transaction.addToBackStack(null)
-        transaction.commit()
     }
 
     override fun onDestroyView() {
